44a3107f413408064cfe43fcc223454095e92c95
nate.young <youngnh@gmail.com>
2009-03-05 10:24:44 -0600

    made vector representation and display the only way of doing things

--- pixels.js
+++ pixels.js
@@ -1,15 +1,5 @@
 function displayImage(data, pixels) {
     var i = 0;
-    for(var y = 0; y < 32; y++) {
-        for(var x = 0; x < 32; x++) {
-            var rgba = data[i++];
-            pixels[y][x].setRGB(rgba.r, rgba.g, rgba.b, rgba.a);
-        }
-    }
-}
-
-function displayImage2(data, pixels) {
-    var i = 0;
     while(i < data.length) {
         var pixel = data[i++];
         var x = pixel[0];
